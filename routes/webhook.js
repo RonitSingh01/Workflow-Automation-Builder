d926a9634554ca4f94ce3c69c92b520858b39d17
ronitsingh12 <singhronit128@gmail.com>
2025-11-13 14:31:57 +0530

    Remove all files and folders (reset branch)

--- routes/webhook.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const webhookController = require('../controllers/webhookController');
-
-// Route to handle Google Form submissions
-router.post('/google-form', webhookController.handleGoogleForm);
-
-module.exports = router;
